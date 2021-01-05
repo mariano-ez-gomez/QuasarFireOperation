e1fc5aa875d313de710cf24947019db787d3ed19
Mariano Ezequiel Gomez <mariano.ez.gomez@gmail.com>
2021-01-05 15:30:41 -0300

    initial commit

---
+++ src/main/java/com/mercadolibre/quasar_fire_operation/QuasarFireOperationApplication.java
@@ -0,0 +1,13 @@
+package com.mercadolibre.quasar_fire_operation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuasarFireOperationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuasarFireOperationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mercadolibre/quasar_fire_operation/QuasarFireOperationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercadolibre.quasar_fire_operation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuasarFireOperationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
